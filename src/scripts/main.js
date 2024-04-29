95ca3e70aa38f1367939255d21a4644d92b0c595
kovachhh <koval.viktor338@gmail.com>
2024-04-29 19:18:40 +0300

    fixed hover bug on 'Call' icon

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const phoneIconElement = document.getElementsByClassName('icon--phone')[0];
+const phoneIconElement = document.getElementById('call');
 const phoneNumberElement = document.getElementsByClassName(
   'top-bar__icons__phone__link',
 )[0];
