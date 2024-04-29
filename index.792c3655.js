aa36bfc2e634da6d22fa0f17748e658897e45e7c
kovachhh <koval.viktor338@gmail.com>
2024-04-29 19:31:54 +0300

    make build

---
+++ index.792c3655.js
@@ -0,0 +1,3 @@
+var e=document.getElementsByClassName("icon--phone")[0],n=document.getElementsByClassName("top-bar__icons__phone__link")[0];e.addEventListener("mouseover",function(){n.classList.remove("hidden")}),e.addEventListener("mouseout",function(){n.classList.add("hidden")});//# sourceMappingURL=index.792c3655.js.map
+
+//# sourceMappingURL=index.792c3655.js.map
