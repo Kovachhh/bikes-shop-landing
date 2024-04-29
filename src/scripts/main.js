67e76e4793aecc94aacfe0a47eca4012b040dbd8
kovachhh <koval.viktor338@gmail.com>
2024-04-29 16:55:28 +0300

    removed unnecessary comments

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,12 +5,10 @@ const phoneNumberElement = document.getElementsByClassName(
   'top-bar__icons__phone__link',
 )[0];
 
-// Додаємо обробник подій для наведення миші на елемент
 phoneIconElement.addEventListener('mouseover', function() {
   phoneNumberElement.classList.remove('hidden');
 });
 
-// Додаємо обробник подій для відведення миші від елемента
 phoneIconElement.addEventListener('mouseout', function() {
   phoneNumberElement.classList.add('hidden');
 });
