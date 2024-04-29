3334af909867ea9c66049479e8222b0a8bcbb203
kovachhh <koval.viktor338@gmail.com>
2024-04-29 16:44:32 +0300

    increased logo in main and side menus, added appearing of phone number after hovering the 'Call' icon, fixed autocomplete styles of inputs

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const phoneIconElement = document.getElementsByClassName('icon icon--phone')[0];
+const phoneNumberElement = document.getElementsByClassName(
+  'top-bar__icons__phone__link',
+)[0];
+
+// Додаємо обробник подій для наведення миші на елемент
+phoneIconElement.addEventListener('mouseover', function() {
+  phoneNumberElement.classList.remove('hidden');
+});
+
+// Додаємо обробник подій для відведення миші від елемента
+phoneIconElement.addEventListener('mouseout', function() {
+  phoneNumberElement.classList.add('hidden');
+});
